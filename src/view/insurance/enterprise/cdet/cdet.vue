0505f3ce2b167b9111c31f394304595bd3485641
Alex69King <33092407+Alex69King@users.noreply.github.com>
2020-03-05 04:07:04 +0800

    更新

--- src/view/insurance/enterprise/cdet/cdet.vue
+++ src/view/insurance/enterprise/cdet/cdet.vue
@@ -38,42 +38,35 @@
       </Col>
       <Col span="1" style='text-align:center'>合同照片</Col>
       <Col span="5">
-      <div class="com-upload-img">
-        <div class="img_group">
-          <div class="img_box" v-if="allowAddImg">
-            <input type="file" accept="image/*" multiple="multiple" @change="changeImg($event)" v-if='isChange'>
-            <div class="filter"></div>
-          </div>
-          <div class="demo-upload-list" v-for="(item,index) in imgArr" :key='index'>
-            <img :src="item" alt="">
-            <div class="demo-upload-list-cover">
-              <Icon type="ios-eye-outline" @click.native="handleView(index)"></Icon>
-              <Icon type="ios-trash-outline" @click.native="deleteImg(index)"></Icon>
-            </div>
-          </div>
-          <!-- <Row>
-            <div class="img_box" v-for="(item,index) in imgArr" :key="index">
-              <Col span="4">
-              <div class="img_show_box">
-                <img :src="item" alt="">
-                <i class="img_delete" @click="deleteImg(index)"></i>
-                </img>
+        <div class="demo-upload-list" v-for="item in uploadList">
+          <template v-if="item.status === 'finished'">
+              <img :src="item.url">
+              <div class="demo-upload-list-cover">
+                  <Icon type="ios-eye-outline" @click.native="handleView(item.name)"></Icon>
+                  <Icon type="ios-trash-outline" @click.native="handleRemove(item)"></Icon>
               </div>
-              </Col>
-            </div>
-          </Row> -->
-        </div>
-        <Modal title="合同文件预览" v-model="visible" width='60%' :styles="{top: '20px'}">
-          <!-- <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%"> -->
-          <Carousel v-model="value1" loop>
-            <CarouselItem v-for='(img,index) in imgArr' :key='index'>
-              <div class="demo-carousel">
-                <img :src="img" style="width: 100%" alt="">
-              </div>
-            </CarouselItem>
-          </Carousel>
-        </Modal>
+          </template>
+          <template v-else>
+              <Progress v-if="item.showProgress" :percent="item.percentage" hide-info></Progress>
+          </template>
       </div>
+      <Upload ref="upload" :show-upload-list="false" :default-file-list="defaultList" :on-success="handleSuccess" :format="['jpg','jpeg','png']" :max-size="2048" :on-format-error="handleFormatError"
+              :on-exceeded-size="handleMaxSize" :before-upload="handleBeforeUpload" :data="{companyid:formValidate2.id,token:token}" multiple type="drag"
+              action="http://47.105.49.81:2222/api/main/updataimg" style="display: inline-block;width:58px;">
+        <div style="width: 58px;height:58px;line-height: 58px;">
+          <Icon type="ios-camera" size="20"></Icon>
+        </div>
+      </Upload>
+      <Modal title="合同文件预览" v-model="visible" width='60%' :styles="{top: '20px'}">
+        <!-- <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%"> -->
+        <Carousel v-if="visible" v-model="value1" loop>
+          <CarouselItem v-for='(img,index) in defaultList' :key='index'>
+            <div class="demo-carousel">
+              <img :src="img.url" style="width: 100%" alt="">
+            </div>
+          </CarouselItem>
+        </Carousel>
+      </Modal>
       </Col>
     </Row>
     <FormItem>
@@ -82,8 +75,15 @@
       <Button icon="md-create" size="large" type="primary" @click="doChange" style="margin-left: 8px">修 改</Button>
     </FormItem>
     <Button size="large" icon="md-add" type="success" @click="showAddModal = true" style="margin:4px">新增保单</Button>
-    <div class="tableList" style="margin:4px">
-      <Table size="large" border stripe :columns="columns" :data="tableLisr" @on-row-dblclick="pdet"></Table>
+    <div class="tableList">
+      <Table size="large" border stripe highlight-row :columns="columns" :data="tableLisr" @on-row-dblclick="pdet">
+        <template slot-scope="{ row }" slot="id">
+          <strong>{{ row.id }}</strong>
+        </template>
+        <template slot-scope="{ row }" slot="action">
+          <Button type="error" @click="remove(row.id)">删除</Button>
+        </template>
+      </Table>
     </div>
     <div class="text-right pageList">
       <Page :total="total" @on-change="changePage" :current.sync="pageNo" :page-size="pageSize" show-total show-elevator />
@@ -134,7 +134,7 @@
         <!-- <FormItem label="邮箱" prop="mail">
                 <Input v-model="formValidate.mail" placeholder="输入电子邮箱"></Input>
                 </FormItem>-->
-        <FormItem label="合同文件" prop="desc">
+        <!-- <FormItem label="合同文件" prop="desc">
           <div class="com-upload-img">
             <div class="img_group">
               <div class="img_box" v-if="allowAddImg">
@@ -159,7 +159,7 @@
               </Carousel>
             </Modal>
           </div>
-        </FormItem>
+        </!--> -->
         <FormItem label="备注" prop="desc">
           <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="输入备注..."></Input>
         </FormItem>
@@ -168,16 +168,20 @@
   </Form>
 </template>
 <script>
+  import { getToken } from '@/libs/util'
   import axios from '@/libs/api.request'
   export default {
     data() {
       return {
         value1: 0,
+        defaultList: [{
+          'name': '7',
+          'url': 'http://47.105.49.81:2222/api/main/getimg/7/95K6GQ7zL2fXaTrD'
+        }],
+        imgName: '',
         visible: false,
         uploadList: [],
         imgData: '',
-        imgArr2: [],
-        imgArr: [],
         imgSrc: '',
         allowAddImg: true,
         allowAddImg2: true,
@@ -200,6 +204,7 @@
         total: 10,
         pageSize: 10,
         pageNo: 1,
+        token: '',
         columns: [
           {
             align: 'center',
@@ -295,6 +300,8 @@
       console.log('创建完成')
       console.log('接收的参数', this.$route.query.value)
       this.getValue = this.$route.query.value
+      this.token = getToken()
+      console.log('token值', this.token)
       let that = this
       axios.request({
         method: 'post',
@@ -322,18 +329,18 @@
       }).catch(function (error) {
         console.log(error)
       })
-      axios.request({
-        method: 'post',
-        url: '/main/getimg',
-        data: {
-          companyid: that.getValue
-        }
-      }).then(function (res) {
-        console.log('请求返回后的企业合同图片', res)
-        that.imgArr = res.data.data
-      }).catch(function (error) {
-        console.log(error)
-      })
+      // axios.request({
+      //   method: 'post',
+      //   url: '/main/getimg',
+      //   data: {
+      //     companyid: that.getValue
+      //   }
+      // }).then(function (res) {
+      //   console.log('请求返回后的企业合同图片', res)
+      //   that.imgArr = res.data.data
+      // }).catch(function (error) {
+      //   console.log(error)
+      // })
       axios.request({
         method: 'post',
         url: '/main/instype'
@@ -429,142 +436,45 @@
         console.log(this.formValidate)
         this.$Message.success('点击确定!')
       },
-      changeImg: function (e) {
-        var _this = this
-        var imgLimit = 1024
-        var files = e.target.files
-        var image = new Image()
-        if (files.length > 0) {
-          var dd = 0
-          var timer = setInterval(function () {
-            if (
-              files.item(dd).type !== 'image/png' &&
-              files.item(dd).type !== 'image/jpeg' &&
-              files.item(dd).type !== 'image/jpg'
-            ) {
-              return false
-            }
-
-            if (files.item(dd).size > imgLimit * 102400) {
-              // to do sth
-            } else {
-              image.src = window.URL.createObjectURL(files.item(dd))
-              image.onload = function () {
-                // 默认按比例压缩
-                var w = image.width
-                var h = image.height
-                // var scale = w / h
-                // w = 200
-                // h = w / scale
-                // 默认图片质量为0.7，quality值越小，所绘制出的图像越模糊
-                var quality = 1
-                // 生成canvas
-                var canvas = document.createElement('canvas')
-                var ctx = canvas.getContext('2d')
-                // 创建属性节点
-                var anw = document.createAttribute('width')
-                anw.nodeValue = w
-                var anh = document.createAttribute('height')
-                anh.nodeValue = h
-                canvas.setAttributeNode(anw)
-                canvas.setAttributeNode(anh)
-                ctx.drawImage(image, 0, 0, w, h)
-                var ext = image.src
-                  .substring(image.src.lastIndexOf('.') + 1)
-                  .toLowerCase() // 图片格式
-                var base64 = canvas.toDataURL('image/' + ext, quality)
-                // 回调函数返回base64的值
-                if (_this.imgArr.length <= 8) {
-                  _this.imgArr.unshift('')
-                  _this.imgArr.splice(0, 1, base64) // 替换数组数据的方法，此处不能使用：this.imgArr[index] = url;
-                  if (_this.imgArr.length >= 9) {
-                    _this.allowAddImg = false
-                  }
-                }
-              }
-            }
-
-            if (dd < files.length - 1) {
-              dd++
-            } else {
-              clearInterval(timer)
-            }
-          }, 1000)
-        }
+      handleSuccess(res, file) {
+        console.log('上传后返回信息', res)
+        file.url = 'http://47.105.49.81:2222/api/main/getimg' + '/' + res.id + '/' + this.token;
+        console.log('图片地址', file.url)
+        file.name = res.id;
       },
-      changeImg2: function (e) {
-        var _this = this
-        var imgLimit = 1024
-        var files = e.target.files
-        var image = new Image()
-        if (files.length > 0) {
-          var dd = 0
-          var timer = setInterval(function () {
-            if (
-              files.item(dd).type !== 'image/png' &&
-              files.item(dd).type !== 'image/jpeg' &&
-              files.item(dd).type !== 'image/jpg'
-            ) {
-              return false
-            }
-
-            if (files.item(dd).size > imgLimit * 102400) {
-              // to do sth
-            } else {
-              image.src = window.URL.createObjectURL(files.item(dd))
-              image.onload = function () {
-                // 默认按比例压缩
-                var w = image.width
-                var h = image.height
-                // var scale = w / h
-                // w = 200
-                // h = w / scale
-                // 默认图片质量为0.7，quality值越小，所绘制出的图像越模糊
-                var quality = 1
-                // 生成canvas
-                var canvas = document.createElement('canvas')
-                var ctx = canvas.getContext('2d')
-                // 创建属性节点
-                var anw = document.createAttribute('width')
-                anw.nodeValue = w
-                var anh = document.createAttribute('height')
-                anh.nodeValue = h
-                canvas.setAttributeNode(anw)
-                canvas.setAttributeNode(anh)
-                ctx.drawImage(image, 0, 0, w, h)
-                var ext = image.src
-                  .substring(image.src.lastIndexOf('.') + 1)
-                  .toLowerCase() // 图片格式
-                var base64 = canvas.toDataURL('image/' + ext, quality)
-                // 回调函数返回base64的值
-                if (_this.imgArr2.length <= 8) {
-                  _this.imgArr2.unshift('')
-                  _this.imgArr2.splice(0, 1, base64) // 替换数组数据的方法，此处不能使用：this.imgArr[index] = url;
-                  if (_this.imgArr2.length >= 9) {
-                    _this.allowAddImg2 = false
-                  }
-                }
-              }
-            }
-
-            if (dd < files.length - 1) {
-              dd++
-            } else {
-              clearInterval(timer)
-            }
-          }, 1000)
-        }
+      handleFormatError(file) {
+        this.$Notice.warning({
+          title: '文件格式不正确',
+          desc: '文件 ' + file.name + ' 的格式不正确，请选择jpg、jpeg或png格式的图片。'
+        });
       },
-      deleteImg: function (index) {
-        this.imgArr.splice(index, 1)
-        if (this.imgArr.length < 9) {
-          this.allowAddImg = true
+      handleMaxSize(file) {
+        this.$Notice.warning({
+          title: '超出文件大小限制',
+          desc: '文件  ' + file.name + ' 超出大小限制，文件最大为2MB。'
+        });
+      },
+      handleBeforeUpload() {
+        const check = this.uploadList.length < 5;
+        if (!check) {
+          this.$Notice.warning({
+            title: '最多可以上传5张图片。'
+          });
         }
+        return check;
+      },
+      handleRemove(file) {
+        const fileList = this.$refs.upload.fileList;
+        this.$refs.upload.fileList.splice(fileList.indexOf(file), 1);
       },
       handleView(index) {
+        console.log(index)
         this.value1 = index
         this.visible = true
       }
+    },
+    mounted() {
+      this.uploadList = this.$refs.upload.fileList;
     }
   }
 </script>
