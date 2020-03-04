d93a2ef8075e71b528e84f62268380d65ea92b3f
AkariMarisa <826436816@qq.com>
2020-03-04 22:54:48 +0800

    新增格式化日期方法

--- src/libs/util.js
+++ src/libs/util.js
@@ -411,3 +411,29 @@ export function copyArray(arr) {
     }
   })
 }
+
+/**
+ * 格式化日期
+ * @param {Date} date 待转换的日期
+ * @param {String} fmt 日期格式
+ */
+export function formatDate(date, fmt) {
+  const o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds(),
+    'q+': Math.floor((date.getMonth() + 3) / 3),
+    'S': date.getMilliseconds()
+  }
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  for (const k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+    }
+  }
+  return fmt
+}
